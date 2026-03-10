db37d69ae168406cda14daf530c49b555bf939f3
Santiago Gomez <Santigomez1129@gmail.com>
2026-03-10 12:43:02 -0300

    New logo added

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <Link to={"/"}>
-      <h1 className="scroll-m-20 text-left p-2 text-4xl text-primary font-extrabold tracking-tight text-balance">
-        F1
-      </h1>
+    <Link
+      to="/"
+      className="shrink-0 inline-flex h-12 items-center justify-center rounded-md  bg-accent pr-3 ml-2 shadow-xs"
+      aria-label="Go to home"
+    >
+      <img
+        src="/f1-logo-trimmed.png"
+        alt="Formula 1 logo"
+        className="h-7 w-auto object-contain"
+      />
     </Link>
   );
 }
